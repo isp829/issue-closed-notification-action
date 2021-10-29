aa543f2fda9481557aa22b529112c291791de30a
MOHANKUMAR T <mohan13081999@gmail.com>
2021-10-29 10:19:30 +0530

    Fix typo in message template

--- issue_closed_notification/MessageTemplates.py
+++ issue_closed_notification/MessageTemplates.py
@@ -1,9 +1,9 @@
 messageTemplates=[
     "assignee hit issue [repo_name#issue_number](issue_url) out of the park!",
-    "assignee just smashed [repo_name#issue_number](issue_url)!",
+    "assignee just smashed [repo_name#issue_number](issue_url) !",
     "assignee showed [repo_name#issue_number](issue_url) who's boss!",
     "[repo_name#issue_number](issue_url) was vanquished by assignee!",
     "[repo_name#issue_number](issue_url) has been promoted to prod thanks to assignee!",
     "[repo_name#issue_number](issue_url) was just shown the definition of expelliarmus by assignee",
-    "assignee just cracked out [repo_name#issue_number](issue_url)!",
+    "assignee just cracked out [repo_name#issue_number](issue_url) !",
 ]
