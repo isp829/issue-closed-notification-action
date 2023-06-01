d5dc249a96d851af77eb3098e68e4d327335ec27
isp829 <isp829@naver.com>
2023-06-01 16:07:11 +0900

    Update Messages.py

--- issue_closed_notification/Messages.py
+++ issue_closed_notification/Messages.py
@@ -4,9 +4,8 @@ from issue_closed_notification import MessageTemplates
 def getRandomTemplate():
     return random.choice(MessageTemplates.messageTemplates)
 
-def getMessage(assignee,repo_name,issue_number,issue_url):
+def getMessage(repo_name,issue_number,issue_url):
     template=getRandomTemplate()
-    message=template.replace("assignee",assignee)
     message=message.replace("repo_name",repo_name)
     message=message.replace("issue_number",issue_number)
     message=message.replace("issue_url",issue_url)
